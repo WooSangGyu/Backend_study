68cd88157012c11e3ef41c65b941122b83d96133
WooSangGyu <wsg0701@naver.com>
2020-06-12 10:04:01 +0900

    AWS S3 Image Upload

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const models = require('./models/index');
 // var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boardRouter = require('./routes/boardControl');
+var awss3Router = require('./routes/awss3');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/', boardRouter);
+app.use('/', awss3Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- backend/routes/awss3.js
+++ backend/routes/awss3.js
@@ -4,7 +4,7 @@ const AWS = require("aws-sdk");
 const path = require("path");
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-AWS.config.loadFromPath(__dirname + "../config/awsconfig.json");
+AWS.config.loadFromPath(__dirname + '/../config/awsconfig.json');
 
 let s3 = new AWS.S3();
 
@@ -20,13 +20,27 @@ let upload = multer({
     })
 })
 
-router.get('/upload', function(req, res, next) {
+router.get('/upload', function(req, res) {
   res.render('upload');
 });
 
 router.post('/upload', upload.single("imgFile"), function(req, res, next) {
     let imgFile = req.file;
+    console.log(imgFile);
     res.json(imgFile);
 })
 
+router.get('/down', function(req, res, next) {
+    res.setHeader("content-type", "some/type");
+    var s3 = new AWS.S3();
+    var params = {Bucket : 'back-end-study',
+            Key : function (req, file, cb) {
+                let extension = path.extname(file.originalname);
+                cb(null, Date.now().toString()+extension)
+            },
+            acl:'public-read-write'
+        };
+    var stream = s3.getObject(params).createReadStream(Key);
+    stream.pipe(res);
+})
 module.exports = router;
